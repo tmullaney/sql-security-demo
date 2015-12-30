c0ce5d7a6bcc053b8cd5a8be680114b5edb8f22d
tmullaney <tmullaney33@gmail.com>
2015-12-29 19:14:16 -0800

    create applicationUser with r/w roles, and run RLS script in post deploy

--- postdeploy.cmd
+++ postdeploy.cmd
@@ -1,5 +1,4 @@
 ECHO Executing postdeploy.cmd
-DIR
 
 ECHO Running sql scripts...
 SET _conn=%SQLAZURECONNSTR_DefaultConnection%
@@ -8,6 +7,10 @@ SET _conn="%_conn:;=";"%"                                   &:: add quotes aroun
 FOR %%a IN (%_conn%) DO @FOR /F %%b IN (%%a) DO SET _%%b    &:: set pieces as variables
 SET _conn=
 
-sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %_UserId% -P %_Password% -i .\Sql\00-Initial-Migrations.sql
+sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %APPSETTING_administratorLogin% -P %APPSETTING_administratorLoginPassword% -Q "CREATE USER %APPSETTING_applicationLogin% WITH PASSWORD = '%APPSETTING_applicationLoginPassword%'"
+sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %APPSETTING_administratorLogin% -P %APPSETTING_administratorLoginPassword% -Q "ALTER ROLE db_datareader ADD MEMBER %APPSETTING_applicationLogin%"
+sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %APPSETTING_administratorLogin% -P %APPSETTING_administratorLoginPassword% -Q "ALTER ROLE db_datawriter ADD MEMBER %APPSETTING_applicationLogin%"
+sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %APPSETTING_administratorLogin% -P %APPSETTING_administratorLoginPassword% -i .\Sql\00-Initial-Migrations.sql
+sqlcmd -S %_DataSource% -d %_InitialCatalog% -U %APPSETTING_administratorLogin% -P %APPSETTING_administratorLoginPassword% -i .\Sql\01-Enable-Row-Level-Security.sql
 
 ECHO Finished executing postdeploy.cmd
