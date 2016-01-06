92f007231feb17075af82b8632adb37012cac1da
tmullaney <thmullan@microsoft.com>
2016-01-06 11:36:50 -0800

    remove audit from azuredeploy

--- ContactManager/Migrations/Configuration.cs
+++ ContactManager/Migrations/Configuration.cs
@@ -16,36 +16,5 @@ namespace ContactManager.Migrations
         {
             AutomaticMigrationsEnabled = false;
         }
-        
-        protected override void Seed(ContactManager.Models.ApplicationDbContext context)
-        {
-            // Add two sample users (via Identity Framework)
-            //var um = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-            //um.Create(new ApplicationUser() { UserName = "user1@contoso.com" }, "user1password");
-            //um.Create(new ApplicationUser() { UserName = "user2@contoso.com" }, "user2password");
-
-            // Run included SQL scripts to seed sample data and enable security features
-            //var baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            //this.executeSqlFile(context, baseDir + "\\sql\\01-Seed-Contacts.sql");
-            //this.executeSqlFile(context, baseDir + "\\sql\\02-Enable-Row-Level-Security.sql");
-        }
-
-        private void executeSqlFile(ContactManager.Models.ApplicationDbContext context, string sqlFile)
-        {
-            // Split "GO"-sepatated batches into separate SqlCommands
-            var sqlScriptText = File.ReadAllText(sqlFile);
-            var batchTexts = Regex.Split(sqlScriptText, "\r\nGO\r\n", RegexOptions.IgnoreCase);
-            foreach (var batchText in batchTexts)
-            {
-                try
-                {
-                    context.Database.ExecuteSqlCommand(batchText);
-                }
-                catch (System.ArgumentException)
-                {
-                    // Ignore empty commands
-                }
-            }
-        }
     }
 }
